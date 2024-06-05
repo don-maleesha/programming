3d863289e445684a498f02784489cf4f87ab4a71
donMaleesha <lochanamt@gmail.com>
2024-06-05 13:41:24 +0530

    escape sequences

---
+++ firstProgramme.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("Hello World!\n");
+	printf("\tWelcome to C programming.\n");
+	printf("\tC is a \"High Level programming language\".\n");
+	printf("\tIt is very friendly to identify the programming essential\b\n\rs.");
+	return 0;
+	
+	//escape sequences
+	
+	/*
+		\n = new line
+		\t = tab
+		\" = double quotes
+		\b = back space (remove one character from the left")
+		\r = return carriage
+		\\ = balck slash
+		\' = single quote
+	*/
+	
+	/*
+		Hello World!
+						Welcome to C programming. 
+						C is a "High Level programming language".
+						It is very friendly to identify the programming essential
+		s.			
+	*/
+}
