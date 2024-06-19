530d5484c6f8eb0ed6c9fee16f92ad9ebcb88abd
donMaleesha <lochanamt@gmail.com>
2024-06-19 11:45:51 +0530

    hands-on programming with programiz

---
+++ programiz_tasks/print_integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	//Program to Print an Integer
+	
+	int number;
+	
+	printf("Enter an integer number: ");
+	scanf("%d", &number);
+	
+	printf("You entered: %d", number);
+	
+	return 0;
+	
+}
