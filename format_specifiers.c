630f2e130a17bcfcae8e53bc6241f78533ba09dd
donMaleesha <lochanamt@gmail.com>
2024-06-19 10:28:26 +0530

    functionality of format specifiers

--- format_specifiers.c
+++ format_specifiers.c
@@ -2,14 +2,31 @@
 
 int main() {
 	
-	int x = 4;
-	float y = 20.42;
-	char z = 'j';
-	
-	printf("x = %d\n", x);
-	printf("y = %f\n", y);
-	printf("z = %c\n", z);
-	
-	return 0;
-	
+    int x = 4;
+    
+    float y = 20.42;
+    
+    char z = 'j';
+    
+    // Declare and initialize a char variable w with the character 'lk'
+    // Multi-character constants are implementation-defined, typically treated as integers
+    // 'lk' will be treated as an integer, but only the least significant byte 'k' will be assigned to w
+    char w = 'lk';
+    
+    // Print the value of x using the %d format specifier for integers
+    printf("x = %d\n", x);
+    
+    // Print the value of y using the %f format specifier for floating-point numbers
+    printf("y = %f\n", y);
+    
+    // Print the value of z using the %c format specifier for characters
+    printf("z = %c\n", z);
+    
+    // Print the value of w using the %c format specifier for characters
+    // This will print 'k' because 'lk' is treated as an integer and the least significant byte 'k' is stored in w
+    printf("w = %c\n", w);
+    
+    // Return 0 to indicate successful completion of the program
+    return 0;
 }
+
