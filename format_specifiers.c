aa3a70111e7d095f789ae8af1a7e1d651d86b734
donMaleesha <lochanamt@gmail.com>
2024-06-19 10:21:14 +0530

    functionality of format specifiers

---
+++ format_specifiers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	int x = 4;
+	float y = 20.42;
+	char z = 'j';
+	
+	printf("x = %d\n", x);
+	printf("y = %f\n", y);
+	printf("z = %c\n", z);
+	
+	return 0;
+	
+}
